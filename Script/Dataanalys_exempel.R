690ecb823d9e717a5a3965e9b249145103918ad3
88305Louise <louia@kth.se>
2026-02-20 10:12:49 +0100

    Sorterat efter män och kvinnor

--- Script/Dataanalys_exempel.R
+++ Script/Dataanalys_exempel.R
@@ -7,6 +7,13 @@ drop <- c("Study.ID","Patient.ID", "Sample.ID", "Number.of.Samples.Per.Patient",
 Glioblastoma_2013_cleaned = Glioblastoma_2013[,!(names(Glioblastoma_2013) %in% drop)]
 View(Glioblastoma_2013_cleaned)
 
-# Group into males and females
+# Check sex correspondance to CIMP
 table(Glioblastoma_2013$Sex,
       Glioblastoma_2013$G.CIMP..Methylation)
+
+# split data for males and females
+data_male <- Glioblastoma_2013_cleaned[Glioblastoma_2013_cleaned$Sex == "Male",]
+data_female <- Glioblastoma_2013_cleaned[Glioblastoma_2013_cleaned$Sex == "Female",]
+
+View(data_female)
+View(data_male)
